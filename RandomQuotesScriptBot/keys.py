003ca4252fe2dffd4f37332722835f915779eedb
Mohammad Ibrahim Khan <97192893+IbyCodes@users.noreply.github.com>
2023-09-20 18:15:59 -0600

    Update keys.py, specified values not required

--- RandomQuotesScriptBot/keys.py
+++ RandomQuotesScriptBot/keys.py
@@ -6,15 +6,15 @@ api_key = ''
 
 api_key_secret = ''
 
-bearer_token = ''
+bearer_token = '' #this value was not used/needed for this project in the main code 
 
 access_token = ''
 
 access_token_secret = ''
 
-client_id = ''
+client_id = ''  #this value was not used/needed for this project in the main code 
 
-client_secret = ''
+client_secret = ''  #this value was not used/needed for this project in the main code 
 
 
 insta_user = '' # instagram username
